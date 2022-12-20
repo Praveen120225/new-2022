0abcfdd7962829c06d66048f85b688013ac6d323
Praveen120225 <118266788+Praveen120225@users.noreply.github.com>
2022-12-20 15:45:05 +0530

    training
    training

---
+++ if and else.js
@@ -0,0 +1,39 @@
+// var num=1
+// if(num % 7== 4){
+//     console.log("true");
+// }
+// else{
+//     console.log("not");
+// }
+// if (num % 7 == 7) {
+//     console.log(" divisible");
+// }
+// else {
+//     console.log(" not divisible");
+// }
+// if(num % 2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+// if(num<0){
+//     console.log("negative");
+// }
+// else{
+//     console.log("positive");
+// }
+
+// let= 2,5,9,14,20,27,35,44
+// var a=2
+// for(let i=0; i<=44; a++){
+//     console.log(i);
+//     i=i+a
+// }
+
+
+// word reverse and letter reverse 
+// let a="pandi karukuvel"
+// let result=a.split(" ").reverse().join(" ")
+// console.log(result);
+
---
+++ while loop.js
@@ -0,0 +1,24 @@
+// let i = 0;
+// while (i <= 18) {
+//     console.log(i)
+
+//     i = i + 2
+// }
+// even number while loop
+// i=2
+// while( i<=10){
+//     console.log(i);
+//    i= i+2
+// }
+// whilw loop 10-1
+// i=10
+// while( i >=0){
+//     console.log(i);
+//     i--
+// }
+// while loop 1-10
+// i=0; 
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
