4167ab6a913a6656780e19e6a7ae4ea90bfa178c
praveen120225 <praveenshiyan12@gmail.com>
2022-12-24 14:14:00 +0530

    ok

---
+++ Javascript/Demo/for loop.js
@@ -0,0 +1,21 @@
+// odd number for loop
+// i=1
+// for(i=1;i<=10;i=i-2){
+//     console.log(i);
+// }
+//15-10
+// i=15
+// for(i=15;i>=10;i=i-1){
+//     console.log(i);
+// }
+//5-15 for loop
+// i=5
+// for(i=5;i<=15;i=i+1){
+//     console.log(i);
+// }
+// let= 2,5,9,14,20,27,35,44
+// var a=2
+// for(let i=0; i<=44; a++){
+//     console.log(i);
+//     i=i+a
+// }
